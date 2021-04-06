594cc7b39370f23d7577d49134a3249d5cc1a865
王刚 <1285454533@qq.com>
2021-04-06 13:53:15 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+// const CompressionWebpackPlugin = require('compression-webpack-plugin');
+// npm run build --report 查看模块划分npm命令
 module.exports = {
     // publicPath: './', // 相对路径
     productionSourceMap: false,
@@ -14,6 +16,22 @@ module.exports = {
             }
         }
     },
+    /* configureWebpack: config => {
+        // gzip
+        config.plugins.push(
+            new CompressionWebpackPlugin({
+                // 正在匹配需要压缩的文件后缀
+                test: /\.(js|css|svg|woff|ttf|json|html)$/,
+                // 大于10kb的会压缩
+                threshold: 10240
+                // 其余配置查看compression-webpack-plugin
+            })
+        )
+        config["performance"] = { //打包文件大小配置
+            "maxEntrypointSize": 10000000,
+            "maxAssetSize": 30000000
+        }
+    }, */
     /* chainWebpack: config => {
         config
             .plugin('webpack-bundle-analyzer')
